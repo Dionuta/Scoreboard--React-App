b37300e52ed7b44d4f175686239f0bf2a660195d
Dionuta <diounta@gmail.com>
2019-03-08 05:27:43 -0800

    phase 2

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,25 +1,4 @@
-const players = [
-    {
-        name: "Guil",
-        score: 50,
-        id: 1
-    },
-    {
-        name: "Treasure",
-        score: 85,
-        id: 2
-    },
-    {
-        name: "Ashley",
-        score: 95,
-        id: 3
-    },
-    {
-        name: "James",
-        score: 80,
-        id: 4
-    }
-];
+
 
 const Header = ({ title, totalPlayers }) => {
     return (
@@ -30,10 +9,11 @@ const Header = ({ title, totalPlayers }) => {
     );
 };
 
-const Player = ({ name }) => {  // used Destructuring to pass name and score prop to counter 
+const Player = ({ name, removePlayer, id }) => {  // used Destructuring to pass name and score prop to counter 
     return (
         <div className='player'>
             <span className='player-name'>
+            <button className="remove-player" onClick={ () => removePlayer(id)}>✖</button>
                 {name}
             </span>
             <Counter
@@ -42,42 +22,82 @@ const Player = ({ name }) => {  // used Destructuring to pass name and score pro
         </div>
     );
 }
+
 class Counter extends React.Component {  // score was passed down from Player 
 
-        state = {
-            score: 0
-        };
-    
+    state = {
+        score: 0
+    };
+
+    incrementScore = () => {   //adds one to score when fired
+        this.setState(prevState => ({
+            score: prevState.score + 1
+        }));
+    }
+
+    decrementScore = () => {//takes one from to score when fired
+        this.setState(prevState => ({
+            score: prevState.score - 1
+        }));
+    }
 
     render() {
         const { score } = this.state;
         return (
             <div className='counter'>
-                <button className='counter-action decrement'> - </button>
+                <button className='counter-action decrement' onClick={this.decrementScore}> - </button>
                 <span className="counter-score">{score}</span>
-                <button className='counter-action increment'> + </button>
+                <button className='counter-action increment' onClick={this.incrementScore}> + </button>
             </div>
         );
     }
 }
-const App = ({ initialPlayers }) => {
-    return (
-        <div className='scoreboard'>
-            <Header
-                title="Scoreboard"
-                totalPlayers={initialPlayers.length}
-            />
+class App extends React.Component {
+    state = {
+        players: [{
+            name: "Guil",
+            id: 1
+        },
+        {
+            name: "Treasure",
+            id: 2
+        },
+        {
+            name: "Ashley",
+            id: 3
+        },
+        {
+            name: "James",
+            id: 4
+        }]
+    }
+     handleRemovePlayer = (id) => {
+       this.setState(prevState => ({
+           players: prevState.players.filter(p => p.id !== id)
+        }))
+     }
 
-            {/* Player List */}
-            {initialPlayers.map(player =>
-                <Player
-                  name={player.name}
-                  id={player.id}
-                  key={player.id.toString()}
+    render() {
+        const{players}= this.state;
+        return (
+            <div className='scoreboard'>
+                <Header
+                    title="Scoreboard"
+                    totalPlayers={players.length}
                 />
-            )}
-        </div>
-    )
+
+                {/* Player List */}
+                {players.map(player =>
+                    <Player
+                        name={player.name}
+                        id={player.id}
+                        key={player.id.toString()}
+                        removePlayer={this.handleRemovePlayer}
+                    />
+                )}
+            </div>
+        );
+    }
 }
 
-ReactDOM.render(<App initialPlayers={players} />, document.getElementById('root')) 
+ReactDOM.render(<App  />, document.getElementById('root')) 
