c4044aa8859e3253814dbf369b7a7fcdb8604b45
Dionuta <diounta@gmail.com>
2019-03-18 23:50:07 -0700

    update stop watch

--- scoreboard/src/components/StopWatch.js
+++ scoreboard/src/components/StopWatch.js
@@ -2,12 +2,17 @@ import React, {Component} from 'react'
 
 
 class StopWatch extends Component {
+    state ={
+        isRunning : flase
+    }
     render() {
         return (
             <div className='stopwatch'>
               <h2>StopWatch</h2>
               <span className='stopwatch-time'>0</span>
-              <button>Start</button>
+              <button>
+                  {this.state.isRunning ? 'Stop':  'Start'}
+              </button>
               <button>Rest</button>
             </div>
        ) 
